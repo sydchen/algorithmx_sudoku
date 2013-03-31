2e3fe477687eb4cd7af9cb50bc9ae3e24515f77a
sydchen <hello.sydd@gmail.com>
2013-03-31 13:53:47 +0800

    update render rule

--- app/controllers/sudoku_controller.rb
+++ app/controllers/sudoku_controller.rb
@@ -8,11 +8,7 @@ class SudokuController < ApplicationController
     respond_to do |format|
       format.json do
         solution_grids = solve_sudoku(params[:grids])
-        if solution_grids
-          render :json => solution_grids
-        else
-          render :nothing => true, :status => 200, :content_type => 'text/html' 
-        end  
+        render :json => solution_grids
       end
     end
   end
